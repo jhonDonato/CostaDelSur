bc413719cb175238c46e5616ffe84eb44473c957
Toto Donato <totodonatozzz@gmail.com>
2025-12-10 00:26:43 +0000

    ahora esta pero porque esta en la parte dercha del cuado quiero que este

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -170,7 +170,7 @@ export default function AdminDashboardPage() {
             <CardDescription>Distribución de las ventas por categoría de platos.</CardDescription>
           </CardHeader>
           <CardContent>
-            <ChartContainer config={{}} className="h-[250px]">
+            <ChartContainer config={{}} className="h-[250px] flex flex-col items-center">
                 <ResponsiveContainer width="100%" height="100%">
                     <RechartsPieChart>
                         <Pie data={categoryData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={60} label>
@@ -179,7 +179,7 @@ export default function AdminDashboardPage() {
                             ))}
                         </Pie>
                         <Tooltip content={<ChartTooltipContent />} />
-                        <Legend layout="vertical" align="right" verticalAlign="middle" wrapperStyle={{paddingLeft: "16px"}}/>
+                        <Legend layout="horizontal" verticalAlign="bottom" align="center" wrapperStyle={{paddingTop: "16px"}}/>
                     </RechartsPieChart>
                 </ResponsiveContainer>
             </ChartContainer>
